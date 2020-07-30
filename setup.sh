420f575bed48508877d11c3b9f8ed979bd1b4966
Emerson Jacobson <emwjacobson@gmail.com>
2020-07-29 17:10:11 -0700

    Added org_url variable

--- setup.sh
+++ setup.sh
@@ -15,6 +15,10 @@ if [ -z "$AGENT_NAME" ]; then
 	read AGENT_NAME
 fi
 
+if [ -z "$ORG_URL" ]; then
+	echo "Enter Orginization URL:"
+	read ORG_URL
+fi
 
 if [ $(uname -m) = "aarch64" ]; then
 	AGENT_URL=https://vstsagentpackage.azureedge.net/agent/2.172.2/vsts-agent-linux-arm64-2.172.2.tar.gz
@@ -33,7 +37,7 @@ tar zxvf vsts-agent.tar.gz -C ./agent/
 echo "Removing compressed file"
 
 cd agent
-./config.sh --unattended --url https://dev.azure.com/emwjacobson --auth pat --token $PAT --pool $POOL_NAME --agent $AGENT_NAME-$RANDOM --acceptTeeEula 
+./config.sh --unattended --url $ORG_URL --auth pat --token $PAT --pool $POOL_NAME --agent $AGENT_NAME-$RANDOM --acceptTeeEula 
 cd ..
 ln -s ./agent/run.sh run.sh
 ln -s ./agent/svc.sh svc.sh
